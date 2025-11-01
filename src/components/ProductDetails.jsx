134ecda86509f3b4a8c698b412d5cf05f15cb9a7
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-01 12:04:31 +0600

    added bided info in product details page

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,5 +1,5 @@
 // src/pages/ProductDetails.jsx
-import React, { use, useRef } from 'react';
+import React, { use, useEffect, useRef, useState } from 'react';
 import { ArrowLeft, MessageCircle, MapPin, Calendar, CheckCircle } from 'lucide-react';
 import { Link, useLoaderData } from 'react-router';
 import { AuthContext } from '../Provider/AuthProvider';
@@ -8,6 +8,7 @@ const ProductDetails = () => {
     const product = useLoaderData()
     const { user } = use(AuthContext)
     const modalbox = useRef(null)
+    const [bids, setBids] = useState([])
     // console.log(product);
     const { image, category, condition, created_at, description, email, location, price_max, price_min, seller_contact, seller_image, seller_name, status, title, usage, _id } = product
 
@@ -41,6 +42,10 @@ const ProductDetails = () => {
                 console.log('after placing bids', data);
                 if (data.insertedId) {
                     alert('bids placed successfully')
+                    newBid._id= data.insertedId
+                    const newBids = [...bids,newBid]
+                    newBids.sort((a,b)=> b.offeredPrice - a.offeredPrice)
+                    setBids(newBids)
                     modalbox.current.close()
                 }
             })
@@ -48,9 +53,18 @@ const ProductDetails = () => {
 
     }
 
+    useEffect(() => {
+        fetch(`http://localhost:3000/products/bids/${_id}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log('bids collection for this product', data);
+                setBids(data)
+            })
+    }, [_id])
+
     return (
         <div className="min-h-screen bg-gray-50 py-8 px-4">
-            <div className="max-w-7xl mx-auto">
+            <div className="w-11/12 mx-auto">
                 {/* Back Button */}
                 <Link to='/allProducts'>
                     <button className="flex items-center gap-2 text-gray-700 hover:text-primary transition mb-6">
@@ -291,6 +305,58 @@ const ProductDetails = () => {
                         </dialog>
 
 
+                    </div>
+                </div>
+
+
+                {/* bids display  */}
+                <div>
+                    <h2 className='font-bold mt-16 mb-5 text-2xl md:text-5xl'>Bids For This Product : <span className='text-primary'>{bids.length}</span></h2>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Buyer Info</th>
+                                    <th>Email</th>
+                                    <th>Bid Price</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            {bids.map((bid, index) =>
+
+                                <tbody>
+                                    {/* row 1 */}
+                                    <tr>
+                                        <th>{index + 1}</th>
+                                        <td>
+                                            <div className="flex items-center gap-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle h-12 w-12">
+                                                        <img
+                                                            src={bid?.buyerImageUrl}
+                                                            alt={bid.buyerName} />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold">{bid.buyerName}</div>
+                                                    <div className="text-sm opacity-50">
+                                                        Bangladesh</div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>{bid.buyerEmail}</td>
+                                        <td>{bid.offeredPrice}</td>
+                                        <th>
+                                            <button className="btn btn-ghost btn-xs">details</button>
+                                        </th>
+                                    </tr>
+                                    {/* row 2 */}
+
+                                </tbody>
+                            )}
+                        </table>
                     </div>
                 </div>
             </div>
